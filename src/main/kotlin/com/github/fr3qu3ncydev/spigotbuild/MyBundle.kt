6179c7a61306d717827cbb39c7117dda57f7225e
GitHub Action <action@github.com>
2024-06-09 17:51:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/fr3qu3ncydev/spigotbuild/MyBundle.kt
+++ src/main/kotlin/com/github/fr3qu3ncydev/spigotbuild/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.fr3qu3ncydev.spigotbuild
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/fr3qu3ncydev/spigotbuild/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/fr3qu3ncydev/spigotbuild/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.fr3qu3ncydev.spigotbuild.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.fr3qu3ncydev.spigotbuild.MyBundle
+import com.github.fr3qu3ncydev.spigotbuild.services.MyProjectService
 import javax.swing.JButton
 
 
